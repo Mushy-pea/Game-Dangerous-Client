1218aa8df0dd582f5d85be6a9cecff92555d8b83
Steven Tinsley <steven.tinsley81@gmail.com>
2023-09-24 23:23:00 +0100

    WallGrid structure now rendered in game board component
    The wall structure of a map can now be rendered in the game board component.

--- UI-components/GameBoard.ts
+++ UI-components/GameBoard.ts
@@ -1,3 +1,5 @@
+import * as API_Types from "../API_Types.js";
+
 type CursorPosition = {
   x : number,
   y : number
@@ -9,19 +11,123 @@ type GridPosition = {
   v : number
 };
 
-// This function draws the game board component grid.
-function drawGrid(gameBoard : CanvasRenderingContext2D,
-                  xMax : number, yMax : number, scale : number) : void {
-  let x = 0, y = 0;
-  gameBoard.strokeStyle = "rgb(0, 0, 192)";
-  gameBoard.fillStyle = "rgb(0, 0, 192)";
-  while (y <= yMax) {
-    gameBoard.strokeRect(x * scale, y * scale, scale, scale);
-    x ++;
-    if (x > xMax) {
-      x = 0;
-      y ++;
+type GridOffset = {
+  uMin : number,
+  uMax : number,
+  vMin : number,
+  vMax : number
+};
+
+type VoxelPoints = {
+  boundary : number[],
+  u1 : number[],
+  u2 : number[],
+  v1 : number[],
+  v2 : number[]
+};
+
+function updateGridOffset(event : KeyboardEvent, gridOffset : GridOffset,
+                          gameBoard : CanvasRenderingContext2D,
+                          mapInterface : API_Types.MapAccessor,
+                          scale : number, mapU_Max : number, mapV_Max : number) : void {
+  const step = 2;
+  let newU_Min = gridOffset.uMin;
+  let newU_Max = gridOffset.uMax;
+  let newV_Min = gridOffset.vMin;
+  let newV_Max = gridOffset.vMax;
+  if (event.code === "KeyW") {
+    newU_Min -= step;
+    newU_Max -= step;
+  }
+  else if (event.code === "KeyS") {
+    newU_Min += step;
+    newU_Max += step;
+  }
+  else if (event.code === "KeyA") {
+    newV_Min -= step;
+    newV_Max -= step;
+  }
+  else if (event.code === "KeyD") {
+    newV_Min += step;
+    newV_Max += step;
+  }
+  else { return }
+
+  if (newU_Min < 0 || newU_Max > mapU_Max || newV_Min < 0 || newV_Max > mapV_Max) { return }
+  else {
+    gridOffset.uMin = newU_Min;
+    gridOffset.uMax = newU_Max;
+    gridOffset.vMin = newV_Min;
+    gridOffset.vMax = newV_Max;
+    gameBoard.fillStyle = "rgb(256, 256, 256)";
+    gameBoard.fillRect(0, 0, scale * 16, scale * 12);
+    drawGrid(gameBoard, mapInterface, gridOffset, 0, 0, scale, "iterative");
+  }
+}
+
+// These two functions handle drawing the game board component grid.
+function generateGrid(u : number, v : number, scale : number, gridOffset : GridOffset)
+                     : VoxelPoints {
+  return {
+    boundary: [(v - gridOffset.vMin) * scale, (u - gridOffset.uMin) * scale, scale, scale],
+    u1: [(v - gridOffset.vMin) * scale, (u - gridOffset.uMin) * scale, scale, scale * 0.2],
+    u2: [(v - gridOffset.vMin) * scale, ((u - gridOffset.uMin) + 0.8) * scale, scale, 0.2 * scale],
+    v1: [(v - gridOffset.vMin) * scale, (u - gridOffset.uMin) * scale, 0.2 * scale, scale],
+    v2: [((v - gridOffset.vMin) + 0.8) * scale, (u - gridOffset.uMin) * scale, 0.2 * scale, scale]
+  };
+}
+
+function drawGrid(gameBoard : CanvasRenderingContext2D, mapInterface : API_Types.MapAccessor,
+                  gridOffset : GridOffset | null, singleU : number, singleV : number,
+                  scale : number, mode : string) : void {
+  function drawVoxel(points : VoxelPoints, voxel : API_Types.WallGrid) : void {
+    gameBoard.strokeRect(points.boundary[0], points.boundary[1],
+                         points.boundary[2], points.boundary[3]);
+    if (voxel.u1_structure) {
+      gameBoard.fillRect(points.u1[0], points.u1[1], points.u1[2], points.u1[3]);
     }
+    else {
+      gameBoard.strokeRect(points.u1[0], points.u1[1], points.u1[2], points.u1[3]);
+    }
+    if (voxel.u2_structure) {
+      gameBoard.fillRect(points.u2[0], points.u2[1], points.u2[2], points.u2[3]);
+    }
+    else {
+      gameBoard.strokeRect(points.u2[0], points.u2[1], points.u2[2], points.u2[3]);
+    }
+    if (voxel.v1_structure) {
+      gameBoard.fillRect(points.v1[0], points.v1[1], points.v1[2], points.v1[3]);
+    }
+    else {
+      gameBoard.strokeRect(points.v1[0], points.v1[1], points.v1[2], points.v1[3]);
+    }
+    if (voxel.v2_structure) {
+      gameBoard.fillRect(points.v2[0], points.v2[1], points.v2[2], points.v2[3]);
+    }
+    else {
+      gameBoard.strokeRect(points.v2[0], points.v2[1], points.v2[2], points.v2[3]);
+    }
+  }
+
+  if (mode === "iterative") {
+    let u = gridOffset.uMin, v = gridOffset.vMin;
+    gameBoard.strokeStyle = "rgb(0, 0, 192)";
+    gameBoard.fillStyle = "rgb(0, 0, 192)";
+    while (u <= gridOffset.uMax) {
+      const points = generateGrid(u, v, scale, gridOffset);
+      const voxel : API_Types.WallGrid = mapInterface.getWallGrid(0, u, v);
+      drawVoxel(points, voxel);
+      v++;
+      if (v > gridOffset.vMax) {
+        v = 0;
+        u++;
+      }
+    }
+  }
+  else {
+    const points = generateGrid(singleU, singleV, scale, gridOffset);
+    const voxel : API_Types.WallGrid = mapInterface.getWallGrid(0, singleU, singleV);
+    drawVoxel(points, voxel);
   }
 }
 
@@ -42,35 +148,66 @@ function mapCursorToGrid(position : CursorPosition, scale : number) : GridPositi
   return {w: 0, u: u, v: v};
 }
 
-function onVoxelHover(gameBoard : CanvasRenderingContext2D, cursorPosition : CursorPosition,
-                      lastVoxelHovered : GridPosition, selectedVoxel : GridPosition, scale : number)
-                      : void {
+function onVoxelHover(gameBoard : CanvasRenderingContext2D, mapInterface : API_Types.MapAccessor,
+                      gridOffset : GridOffset, lastVoxelHovered : GridPosition,
+                      selectedVoxel : GridPosition, cursorPosition : CursorPosition, scale : number)
+                     : void {
   const gridPosition = mapCursorToGrid(cursorPosition, scale);
+  const gridPositionRend = {u: gridPosition.u, v: gridPosition.v};
+  const lastVoxelHoveredRend = {
+    u: lastVoxelHovered.u - gridOffset.uMin,
+    v: lastVoxelHovered.v - gridOffset.vMin
+  };
+  const selectedVoxelRend = {
+    u: selectedVoxel.u - gridOffset.uMin,
+    v: selectedVoxel.v - gridOffset.vMin
+  };
+  gridPosition.u += gridOffset.uMin;
+  gridPosition.v += gridOffset.vMin;
+  console.log(`gridPosition.u: ${gridPosition.u} gridPosition.v: ${gridPosition.v}`);
+  console.log(`lastVoxelHovered.u: ${lastVoxelHovered.u} lastVoxelHovered.v: ${lastVoxelHovered.v}`);
   if (gridPosition.u !== lastVoxelHovered.u || gridPosition.v !== lastVoxelHovered.v) {
-    gameBoard.fillStyle = "rgb(0, 0, 192)";
-    gameBoard.fillRect(gridPosition.v * scale, gridPosition.u * scale, scale, scale);
+    gameBoard.fillStyle = "rgba(0, 0, 192, 0.5)";
+    gameBoard.fillRect(gridPositionRend.v * scale, gridPositionRend.u * scale, scale, scale);
     gameBoard.fillStyle = "rgb(256, 256, 256)";
-    gameBoard.fillRect(lastVoxelHovered.v * scale, lastVoxelHovered.u * scale, scale, scale);
-    gameBoard.strokeRect(lastVoxelHovered.v * scale, lastVoxelHovered.u * scale, scale, scale);
-    if (selectedVoxel.u >= 0) {
-      gameBoard.fillStyle = "rgb(192, 0, 0)";
-      gameBoard.fillRect(selectedVoxel.v * scale, selectedVoxel.u * scale, scale, scale);
-    }
+    gameBoard.fillRect(lastVoxelHoveredRend.v * scale, lastVoxelHoveredRend.u * scale, scale, scale);
+    gameBoard.fillStyle = "rgb(0, 0, 192)";
+    drawGrid(gameBoard, mapInterface, gridOffset, lastVoxelHovered.u, lastVoxelHovered.v, scale,
+      "singular");
+    gameBoard.fillStyle = "rgb(256, 256, 256)";
+    gameBoard.fillRect(selectedVoxelRend.v * scale, selectedVoxelRend.u * scale, scale, scale);
+    gameBoard.fillStyle = "rgb(0, 0, 192)";
+    drawGrid(gameBoard, mapInterface, gridOffset, selectedVoxel.u, selectedVoxel.v, scale,
+             "singular");
+    gameBoard.fillStyle = "rgba(0, 192, 0, 0.5)";
+    gameBoard.fillRect(selectedVoxelRend.v * scale, selectedVoxelRend.u * scale, scale, scale);
     lastVoxelHovered.u = gridPosition.u;
     lastVoxelHovered.v = gridPosition.v;
   }
 }
 
-function selectVoxel(gameBoard : CanvasRenderingContext2D, lastVoxelHovered : GridPosition,
-                     selectedVoxel : GridPosition, scale : number) : void {
-  gameBoard.fillStyle = "rgb(192, 0, 0)";
-  gameBoard.fillRect(lastVoxelHovered.v * scale, lastVoxelHovered.u * scale, scale, scale);
+function selectVoxel(gameBoard : CanvasRenderingContext2D, mapInterface : API_Types.MapAccessor,
+                     gridOffset : GridOffset, lastVoxelHovered : GridPosition,
+                     selectedVoxel : GridPosition, scale : number)
+                    : void {
+  const lastVoxelHoveredRend = {
+    u: lastVoxelHovered.u - gridOffset.uMin,
+    v: lastVoxelHovered.v - gridOffset.vMin
+  };
+  const selectedVoxelRend = {
+    u: selectedVoxel.u - gridOffset.uMin,
+    v: selectedVoxel.v - gridOffset.vMin
+  };
+  gameBoard.fillStyle = "rgba(0, 192, 0, 0.5)";
+  gameBoard.fillRect(lastVoxelHoveredRend.v * scale, lastVoxelHoveredRend.u * scale, scale, scale);
   gameBoard.fillStyle = "rgb(256, 256, 256)";
-  gameBoard.fillRect(selectedVoxel.v * scale, selectedVoxel.u * scale, scale, scale);
-  gameBoard.strokeRect(selectedVoxel.v * scale, selectedVoxel.u * scale, scale, scale);
+  gameBoard.fillRect(selectedVoxelRend.v * scale, selectedVoxelRend.u * scale, scale, scale);
+  gameBoard.fillStyle = "rgb(0, 0, 192)";
+  drawGrid(gameBoard, mapInterface, gridOffset, selectedVoxel.u, selectedVoxel.v, scale, "singular");
   selectedVoxel.u = lastVoxelHovered.u;
   selectedVoxel.v = lastVoxelHovered.v;
+  console.log(`selectedVoxel.u: ${selectedVoxel.u} selectedVoxel.v: ${selectedVoxel.v}`);
 }
 
-export { captureCursor, drawGrid, onVoxelHover, selectVoxel };
+export { updateGridOffset, captureCursor, drawGrid, onVoxelHover, selectVoxel };
 
--- index.ts
+++ index.ts
@@ -9,41 +9,45 @@ async function main() {
   const mapInterface = await ServerInterface.loadMap(
     mapDim.uMaxWall, mapDim.vMaxWall, mapDim.uMaxFloor, mapDim.vMaxFloor
   );
-  console.log(`Wall_grid (0, 10, 10): ${JSON.stringify(mapInterface.getWallGrid(0, 10, 10))}`);
-  console.log(`Floor_grid (0, 5, 5): ${JSON.stringify(mapInterface.getFloorGrid(0, 5, 5))}`);
-  console.log(`Obj_grid (0, 10, 10): ${JSON.stringify(mapInterface.getObjGrid(0, 10, 10))}`);
-  const setSuccess0 = await mapInterface.setWallGridStructure(0, 10, 10, true, true, false, false);
-  if (setSuccess0 === false) { console.log("mapInterface.setWallGridStructure failed") }
 
-  const setSuccess1 = await mapInterface.setWallGridTextures(0, 10, 10, 128, 129, 130, 131);
-  if (setSuccess1 === false) { console.log("mapInterface.setWallGridTextures failed") }
+  // Test code to confirm that the client to server interface is working as expected.
+  // console.log(`Wall_grid (0, 10, 10): ${JSON.stringify(mapInterface.getWallGrid(0, 10, 10))}`);
+  // console.log(`Floor_grid (0, 5, 5): ${JSON.stringify(mapInterface.getFloorGrid(0, 5, 5))}`);
+  // console.log(`Obj_grid (0, 10, 10): ${JSON.stringify(mapInterface.getObjGrid(0, 10, 10))}`);
+  // const setSuccess0 = await mapInterface.setWallGridStructure(0, 10, 10, true, true, false, false);
+  // if (setSuccess0 === false) { console.log("mapInterface.setWallGridStructure failed") }
 
-  const setSuccess2 = await mapInterface.setObjPlace(0, 10, 10, 132, 133, 134, 135, 136, 137, 138);
-  if (setSuccess2 === false) { console.log("mapInterface.setObjPlace failed") }
+  // const setSuccess1 = await mapInterface.setWallGridTextures(0, 10, 10, 128, 129, 130, 131);
+  // if (setSuccess1 === false) { console.log("mapInterface.setWallGridTextures failed") }
+
+  // const setSuccess2 = await mapInterface.setObjPlace(0, 10, 10, 132, 133, 134, 135, 136, 137, 138);
+  // if (setSuccess2 === false) { console.log("mapInterface.setObjPlace failed") }
   
-  const setSuccess3 = await mapInterface.setFloorGrid(0, 5, 5, 139, "Open");
-  if (setSuccess3 === false) { console.log("mapInterface.setFloorGrid failed") }
+  // const setSuccess3 = await mapInterface.setFloorGrid(0, 5, 5, 139, "Open");
+  // if (setSuccess3 === false) { console.log("mapInterface.setFloorGrid failed") }
 
-  const setSuccess4 = await mapInterface.setObjGrid(0, 10, 10, 3, [140, 141, 142, 143]);
-  if (setSuccess4 === false) { console.log("mapInterface.setObjGrid failed") }
-  
-  console.log(`Wall_grid (0, 10, 10): ${JSON.stringify(mapInterface.getWallGrid(0, 10, 10))}`);
-  console.log(`Floor_grid (0, 5, 5): ${JSON.stringify(mapInterface.getFloorGrid(0, 5, 5))}`);
-  console.log(`Obj_grid (0, 10, 10): ${JSON.stringify(mapInterface.getObjGrid(0, 10, 10))}`);
+  // const setSuccess4 = await mapInterface.setObjGrid(0, 10, 10, 3, [140, 141, 142, 143]);
+  // if (setSuccess4 === false) { console.log("mapInterface.setObjGrid failed") }
+  // console.log(`Wall_grid (0, 10, 10): ${JSON.stringify(mapInterface.getWallGrid(0, 10, 10))}`);
+  // console.log(`Floor_grid (0, 5, 5): ${JSON.stringify(mapInterface.getFloorGrid(0, 5, 5))}`);
+  // console.log(`Obj_grid (0, 10, 10): ${JSON.stringify(mapInterface.getObjGrid(0, 10, 10))}`);
+
+  const canvas = <HTMLCanvasElement>document.getElementById("gameBoard");
+  const gameBoard = canvas.getContext("2d");
+  const cursorPosition = {x: 0, y: 0};
+  const lastVoxelHovered = {w: 0, u: 0, v: 0};
+  const selectedVoxel = {w: 0, u: 0, v: 0};
+  const scale = 75;
+  const gridOffset = {uMin: 0, uMax: 11, vMin: 0, vMax: 15};
+  document.onmousemove = event => GameBoard.captureCursor(canvas, event, cursorPosition);
+  document.onmousedown = event => GameBoard.selectVoxel(gameBoard, mapInterface, gridOffset,
+    lastVoxelHovered, selectedVoxel, scale);
+  document.onkeydown = event => GameBoard.updateGridOffset(event, gridOffset, gameBoard,
+    mapInterface, scale, mapDim.uMaxWall, mapDim.vMaxWall);
+  GameBoard.drawGrid(gameBoard, mapInterface, gridOffset, 0, 0, scale, "iterative");
+  setInterval(GameBoard.onVoxelHover, 40, gameBoard, mapInterface, gridOffset, lastVoxelHovered,
+              selectedVoxel, cursorPosition, scale);
 }
 
 main();
-
-const canvas = <HTMLCanvasElement>document.getElementById("gameBoard");
-const gameBoard = canvas.getContext("2d");
-const cursorPosition = {x: 0, y: 0};
-const lastVoxelHovered = {w: 0, u: 0, v: 0};
-const selectedVoxel = {w: 0, u: -1, v: 0};
-
-document.onmousemove = event => GameBoard.captureCursor(canvas, event, cursorPosition);
-document.onmousedown = event => GameBoard.selectVoxel(gameBoard, lastVoxelHovered, selectedVoxel,
-                                                      128);
-GameBoard.drawGrid(gameBoard, 9, 9, 128);
-setInterval(GameBoard.onVoxelHover, 40, gameBoard, cursorPosition, lastVoxelHovered, selectedVoxel,
-            128);
 
